85c0a2c46379d76d737354abe830648993c5b1b5
bitgiga <bitgiga97@gmail.com>
2023-03-20 13:59:10 +0400

    added advices

--- app/main.js
+++ app/main.js
@@ -8,6 +8,18 @@ const adviceList = [
     "Focus on the present moment.",
     "Stay positive and keep moving forward.",
     "Learn from your mistakes and failures.",
+    "If your hair is thinning, try dying your hair a similar tone to your scalp.",
+    "Step 1. Give a shit. Step 2. Don't be a dick. Step 3. Know when to let go.",
+    "Age is of no importance, unless you are a cheese.",
+    "Vinegar is a powerful cleaning agent.",
+    "Accept advice.",
+    "Giving someone a hug can be mutually rewarding. Try to give at least one hug a day to someone.",
+    "Sometimes it's best to ignore other people's advice.",
+    "To cleanly remove the seed from an Avocado, lay a knife firmly across it, and twist.",
+    "Lemon and salt works wonders on tarnished brass.",
+    "Life is better when you sing about bananas.",
+    "Respect other people's opinions, even when they differ from your own.",
+
 
     "Don't be afraid to ask for help when you need it."
 ];
